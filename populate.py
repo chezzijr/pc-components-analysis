10ec54eae2c40ef7241c3dea47d1eb520b9618cd
chezzijr <nhuthuan0501@gmail.com>
2024-11-07 18:03:40 +0700

    feat: script to insert data to postgres

--- populate.py
+++ populate.py
@@ -1,23 +1,26 @@
-from collections import namedtuple
 from sqlalchemy import Engine
 from sqlalchemy.orm import Session
-from parts import CPU, VGA, RAM, Component
+from parts import CPU, VGA, RAM, Mainboard
 from engine import engine
 import pandas as pd
 import numpy as np
 
-Tables = namedtuple("Tables", ["constructor", "ignore_columns"])
-
 csv_to_constructor_map = {
-    "cpu.csv": Tables(CPU, ["price"]),
-    "video-card.csv": Tables(VGA, ["price"]),
+    "cpu.csv": CPU,
+    "video-card.csv": VGA,
+    "motherboard.csv": Mainboard,
+    "memory.csv": RAM,
 }
 
 def populate(engine: Engine):
-    parts = (
-        pd.read_csv(f"./datasets/csv/cpu.csv")
-        .drop("price", axis=1)
-        .replace(np.nan, None)
-    )
+    with Session(engine) as session:
+        for csv, constructor in csv_to_constructor_map.items():
+            data = pd.read_csv(f"./datasets/refined/{csv}")
+            data = data.replace({np.nan: None})
+            for _, row in data.iterrows():
+                component = constructor(**row.to_dict())
+                session.add(component)
+        session.commit()
+    print("Data has been populated")
 
 print(populate(engine))
