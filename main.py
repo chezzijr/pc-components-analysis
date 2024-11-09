140356ba4e75a60a01c1deee3f98e4fed0de2e19
chezzijr <nhuthuan0501@gmail.com>
2024-11-09 10:24:08 +0700

    feat: cli argument

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import asyncio
 import numpy as np
 import pandas as pd
+import argparse as ap
+import logging
+import sys
+from populate import insert_data, insert_prices
 from parts import CPU
 from parts import VGA
 from langchain.schema import StrOutputParser
@@ -12,67 +17,91 @@ from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import PromptTemplate
 from os.path import exists
 
-csv_to_constructor_map = {
-    "cpu.csv": CPU,
-    "video-card.csv": VGA,
-}
+# stdout as the default logging handler
+logfile = "log.txt"
+logging.basicConfig(filename=logfile,
+                    filemode='a',
+                    format='%(asctime)s - %(name)s [%(levelname)s]: %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
 
-documents = []
-for csv_file, constructor in csv_to_constructor_map.items():
-    parts = (
-        pd.read_csv(f"./datasets/csv/{csv_file}")
-        .drop("price", axis=1)
-        .replace(np.nan, None)
-    )
-    # print(parts.isnull().sum())
-    documents.extend(
-        Document(page_content=str(part))
-        for part in parts.apply(lambda row: constructor(*row), axis=1)
-    )
+parser = ap.ArgumentParser()
+parser.add_argument("task", help="The task to be performed which is either insert_data|query_prices|chat")
+args = parser.parse_args()
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-splits = text_splitter.split_documents(documents)
+match args.task:
+    case "insert_data":
+        insert_data()
+        print("Data inserted")
+    case "query_prices":
+        asyncio.run(insert_prices())
+        print("Prices inserted")
+    case "chat":
+        print("Starting the chatbot")
+    case _:
+        print("Invalid task")
 
-if exists("./chroma_langchain_db"):
-    vectorstore = Chroma(
-        collection_name="pc_parts",
-        persist_directory="./chroma_langchain_db",
-        embedding_function=OllamaEmbeddings(model="nomic-embed-text"),
-    )
-else:
-    vectorstore = Chroma.from_documents(
-        documents=splits,
-        embedding=OllamaEmbeddings(model="nomic-embed-text"),
-        persist_directory="./chroma_langchain_db",
-        collection_name="pc_parts",
-    )
+exit(0)
+# csv_to_constructor_map = {
+#     "cpu.csv": CPU,
+#     "video-card.csv": VGA,
+# }
 
-retriever = vectorstore.as_retriever()
-prompt = PromptTemplate.from_template(
-    (
-        "You are an assistant for question-answering tasks. "
-        "Use the following pieces of retrieved context to answer the question. "
-        "If you don't know the answer, just say that you don't know. "
-        "Use three sentences maximum and keep the answer concise.\n"
-        "Question: {question} \n"
-        "Context: {context} \n"
-        "Answer:"
-    )
-)
-llm = OllamaLLM(model="llama3.2:1b", temperature=0.1)
+# documents = []
+# for csv_file, constructor in csv_to_constructor_map.items():
+#     parts = (
+#         pd.read_csv(f"./datasets/csv/{csv_file}")
+#         .drop("price", axis=1)
+#         .replace(np.nan, None)
+#     )
+#     # print(parts.isnull().sum())
+#     documents.extend(
+#         Document(page_content=str(part))
+#         for part in parts.apply(lambda row: constructor(*row), axis=1)
+#     )
 
-def format_docs(documents):
-    return "\n\n".join(doc.page_content for doc in documents)
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+# splits = text_splitter.split_documents(documents)
+
+# if exists("./chroma_langchain_db"):
+#     vectorstore = Chroma(
+#         collection_name="pc_parts",
+#         persist_directory="./chroma_langchain_db",
+#         embedding_function=OllamaEmbeddings(model="nomic-embed-text"),
+#     )
+# else:
+#     vectorstore = Chroma.from_documents(
+#         documents=splits,
+#         embedding=OllamaEmbeddings(model="nomic-embed-text"),
+#         persist_directory="./chroma_langchain_db",
+#         collection_name="pc_parts",
+#     )
+
+# retriever = vectorstore.as_retriever()
+# prompt = PromptTemplate.from_template(
+#     (
+#         "You are an assistant for question-answering tasks. "
+#         "Use the following pieces of retrieved context to answer the question. "
+#         "If you don't know the answer, just say that you don't know. "
+#         "Use three sentences maximum and keep the answer concise.\n"
+#         "Question: {question} \n"
+#         "Context: {context} \n"
+#         "Answer:"
+#     )
+# )
+# llm = OllamaLLM(model="llama3.2:1b", temperature=0.1)
 
 
-rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt
-    | llm
-    | StrOutputParser()
-)
+# def format_docs(documents):
+#     return "\n\n".join(doc.page_content for doc in documents)
 
-result = rag_chain.invoke(
-    input()
-)
-print(result)
+
+# rag_chain = (
+#     {"context": retriever | format_docs, "question": RunnablePassthrough()}
+#     | prompt
+#     | llm
+#     | StrOutputParser()
+# )
+
+# result = rag_chain.invoke(input())
+# print(result)
