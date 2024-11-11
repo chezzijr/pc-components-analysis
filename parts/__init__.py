350d40de6f502b1c37565be1c797faf2bd09808b
chezzijr <nhuthuan0501@gmail.com>
2024-11-11 12:30:06 +0700

    chore: export

--- parts/__init__.py
+++ parts/__init__.py
@@ -1,5 +1,7 @@
 from .cpu import CPU
 from .vga import VGA
 from .ram import RAM
+from .ssd import SSD
+from .hdd import HDD
 from .mainboard import Mainboard
 from .base import Component, Base, Price
