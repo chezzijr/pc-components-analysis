6853808e8ccba1e292237f4988a1d130b60273bc
chezzijr <nhuthuan0501@gmail.com>
2024-11-16 00:23:56 +0700

    feat: chat only when data is inserted

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import asyncio
-import argparse as ap
 import logging
 from chat import chat
-from populate import insert_data, insert_prices
+from populate import insert_components, insert_prices
+from settings import settings
 
 # stdout as the default logging handler
 logfile = "log.txt"
@@ -11,25 +11,11 @@ logging.basicConfig(
     filemode="a",
     format="%(asctime)s - %(name)s [%(levelname)s]: %(message)s",
     datefmt="%H:%M:%S",
-    level=logging.DEBUG,
+    level=logging.INFO,
 )
 
-parser = ap.ArgumentParser()
-parser.add_argument(
-    "task",
-    help="The task to be performed",
-    choices=["insert_data", "query_prices", "chat"],
-)
-args = parser.parse_args()
-
-match args.task:
-    case "insert_data":
-        insert_data()
-        print("Data inserted")
-    case "query_prices":
-        asyncio.run(insert_prices())
-        print("Prices inserted")
-    case "chat":
-        chat()
-    case _:
-        print("Invalid task")
+if not settings.ignore_insert_components:
+    insert_components()
+if not settings.ignore_insert_prices:
+    asyncio.run(insert_prices())
+chat()
