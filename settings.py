7d2d6cf27bb24d48638387f08d786800f8746a08
chezzijr <nhuthuan0501@gmail.com>
2024-11-16 00:22:44 +0700

    feat: add config to control the insertion of data

--- settings.py
+++ settings.py
@@ -3,6 +3,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     postgres_dsn: PostgresDsn
+    ignore_insert_components: bool | None = None
+    ignore_insert_prices: bool | None = None
+
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 settings = Settings()
