412c2218c9249dc863b514c2d28cc35a8de15173
chezzijr <nhuthuan0501@gmail.com>
2024-11-18 10:26:53 +0700

    feat: streamlit web

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import asyncio
 import logging
-from chat import chat
+from langchain.schema import AIMessage, HumanMessage
+import streamlit as st
+import os
 from populate import insert_components, insert_prices
 from settings import settings
+from chat import rag_chain
 
 # stdout as the default logging handler
 logfile = "log.txt"
@@ -16,6 +19,32 @@ logging.basicConfig(
 
 if not settings.ignore_insert_components:
     insert_components()
+    # set env
+    os.environ["ignore_insert_components"] = "true"
 if not settings.ignore_insert_prices:
     asyncio.run(insert_prices())
-chat()
+    os.environ["ignore_insert_prices"] = "true"
+
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
+
+chain = rag_chain.pick("answer")
+st.set_page_config(page_title="PC Components Assistant")
+st.title("Say something...")
+messages = st.container()
+for message in st.session_state["chat_history"]:
+    if isinstance(message, HumanMessage):
+        messages.chat_message("user").write(message.content)
+    elif isinstance(message, AIMessage):
+        messages.chat_message("assistant").write(message.content)
+
+if prompt := st.chat_input("Say something"):
+    messages.chat_message("user").write(prompt)
+    with st.spinner("Generating response..."):
+        resp = messages.chat_message("assistant").write_stream(
+            chain.stream({"input": prompt, "chat_history": st.session_state["chat_history"]})
+        )
+    st.session_state["chat_history"].extend([
+        HumanMessage(content=prompt),
+        AIMessage(content=resp),
+    ])
